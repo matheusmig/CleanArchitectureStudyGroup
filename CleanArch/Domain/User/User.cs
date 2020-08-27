c8f6179ed00bbf24ff45c3cc9ab9abbfd97d3646
Matheus Mignoni <matheus.mignoni@poatek.com>
2020-08-27 17:03:52 -0300

    Created User Model

---
+++ CleanArch/Domain/User/User.cs
@@ -0,0 +1,11 @@
+﻿using Domain.User.ValueObjects;
+
+namespace Domain.User
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public Name Name { get; set; }
+        public Birthdate Birthdate { get; set; }
+    }
+}

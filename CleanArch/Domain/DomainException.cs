0d6eaff1a41c42f32606765bc2f79f7247c9b37c
Matheus Mignoni <matheus.mignoni@poatek.com>
2020-08-27 17:04:26 -0300

    Created UserService

---
+++ CleanArch/Domain/DomainException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Domain
+{
+    public class DomainException : Exception
+    {
+        public DomainException(string businessMessage) 
+            : base(businessMessage)
+        {
+
+        }
+    }
+}
---
+++ CleanArch/Domain/User/UserService.cs
@@ -0,0 +1,27 @@
+﻿using Domain.User.ValueObjects;
+using System.Threading.Tasks;
+
+namespace Domain.User
+{
+    public class UserService
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IUserFactory _userFactory;
+       
+        public UserService(
+            IUserRepository userRepository,
+            IUserFactory userFactory)
+        {
+            _userRepository = userRepository;
+            _userFactory = userFactory;
+        }
+
+        public async Task<User> CreateNewUser(Name name, Birthdate birthdate)
+        {
+            var user = _userFactory.NewUser(name, birthdate);
+            await _userRepository.AddAsync(user);
+
+            return user;
+        }
+    }
+}

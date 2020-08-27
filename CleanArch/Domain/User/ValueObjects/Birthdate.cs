6dd1c945b9396814b09a8af56b91edaf0780f6ab
Matheus Mignoni <matheus.mignoni@poatek.com>
2020-08-27 17:03:40 -0300

    Created User ValueObjects

---
+++ CleanArch/Domain/User/ValueObjects/Birthdate.cs
@@ -0,0 +1,22 @@
+﻿using Domain.User.Exceptions;
+using System;
+
+namespace Domain.User.ValueObjects
+{
+    public readonly struct Birthdate
+    {
+        public Birthdate(DateTime datetime)
+        {
+            var now = DateTime.UtcNow;
+            if (datetime < now.AddYears(18))
+                throw new BirthdateMinorException(datetime);
+
+            if (datetime < now.AddYears(150))
+                throw new BirthdateImmortalException(datetime);
+
+            DateTime = datetime;
+        }
+
+        public DateTime DateTime { get; }
+    }
+}
---
+++ CleanArch/Domain/User/ValueObjects/Name.cs
@@ -0,0 +1,47 @@
+﻿using Domain.User.Exceptions;
+using System.Linq;
+
+namespace Domain.User.ValueObjects
+{
+    public readonly struct Name
+    {
+        public Name(string fullname)
+        {
+            if (!(fullname?.Length > 0))
+                throw new NameLengthException(fullname ?? "null");
+
+            if (!fullname.Contains(' '))
+                throw new NameBadFormatException(fullname);
+            
+            var splittedName = fullname.Split(' ');
+            Filename = splittedName[0];
+            Lastname = splittedName[^1];
+
+            if (splittedName.Length > 2)
+                Middlename = string.Join(' ', splittedName.Skip(1).SkipLast(1));
+            else
+                Middlename = string.Empty;
+        }
+
+        public Name(string firstname, string lastname, string middlename = null)
+        {
+            if (firstname?.Length < 0)
+                throw new NameLengthException(firstname ?? "null");
+
+            if (lastname?.Length < 0)
+                throw new NameLengthException(lastname ?? "null");
+
+            Filename = firstname;
+            Lastname = lastname;
+
+            if (middlename == null)
+                Middlename = string.Empty;
+            else
+                Middlename = middlename;
+        }
+
+        public string Filename { get; }
+        public string Lastname { get; }
+        public string Middlename { get; }
+    }
+}
